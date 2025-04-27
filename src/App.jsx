d1d68a6302b2d092978876a237a3089521973de5
Thrillonek <jindrich.kraina@gmail.com>
2025-04-27 12:15:46 +0200

    New portfolio project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
 
 function App() {
-  const [count, setCount] = useState(0)
+	// const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<nav>
+				<div className='flex items-center gap-4'>
+					<img src='/logo.svg' alt='Logo' className='w-12' />
+					<h1 className='text-3xl serif'>Jindřich Kraina</h1>
+				</div>
+
+				<div className='text-xl'>
+					<Link to='/reference'>Reference</Link>
+				</div>
+			</nav>
+			<main>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/reference' element={<h1>Reference</h1>} />
+				</Routes>
+			</main>
+		</>
+	);
 }
 
-export default App
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react';
+
+function Home() {
+	return (
+		<div className='template'>
+			<h1>Welcome to my portfolio</h1>
+			<p>This is a basic React template.</p>
+		</div>
+	);
+}
+
+export default Home;

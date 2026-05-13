09581083f2422de84ccc7acef63549e39c452fc0
Thrillonek <jindrich.kraina@gmail.com>
2026-05-13 18:54:58 +0200

    Moved ProjectCard component to separate file

---
+++ app/ProjectCard.tsx
@@ -0,0 +1,37 @@
+import { Icon } from '@iconify/react';
+import Image, { StaticImageData } from 'next/image';
+import Link from 'next/link';
+
+type ProjectCardProps = {
+	title: string;
+	description: string;
+	image: StaticImageData;
+	tags: string[];
+	link: string;
+};
+
+export function ProjectCard({ title, description, image, tags, link }: ProjectCardProps) {
+	return (
+		<div className='bg-darker p-4 border border-gray rounded-lg w-md max-w-full layout-col-4'>
+			<Image src={image} alt={'Obrázek projektu ' + title} className='rounded-lg w-full h-auto object-contain shrink-0' />
+			<div className='layout-col-1'>
+				<h4>{title}</h4>
+				<p>{description}</p>
+			</div>
+			<div className='flex gap-1'>
+				{tags.map((tag) => (
+					<span key={tag} className='bg-dark shadow px-2 py-1 rounded-full text-light-gray text-xs'>
+						{tag}
+					</span>
+				))}
+			</div>
+			<div className='flex gap-2'>
+				<button className='button'>See more</button>
+				<Link className='button secondary' href={link} target='_blank' rel='noopener noreferrer'>
+					<span>Go to the page</span>
+					<Icon icon='mingcute:external-link-line' />
+				</Link>
+			</div>
+		</div>
+	);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Image, { StaticImageData } from 'next/image';
 import Link from 'next/link';
 import './base.scss';
 import HobbyCard from './HobbyCard';
+import { ProjectCard } from './ProjectCard';
 
 export default function Home() {
 	return (
@@ -87,39 +88,5 @@ export default function Home() {
 				</div>
 			</section>
 		</main>
-	);
-}
-
-type ProjectCardProps = {
-	title: string;
-	description: string;
-	image: StaticImageData;
-	tags: string[];
-	link: string;
-};
-
-function ProjectCard({ title, description, image, tags, link }: ProjectCardProps) {
-	return (
-		<div className='bg-darker p-4 border border-gray rounded-lg w-md max-w-full layout-col-4'>
-			<Image src={image} alt={'Obrázek projektu ' + title} className='rounded-lg w-full h-auto object-contain shrink-0' />
-			<div className='layout-col-1'>
-				<h4>{title}</h4>
-				<p>{description}</p>
-			</div>
-			<div className='flex gap-1'>
-				{tags.map((tag) => (
-					<span key={tag} className='bg-dark shadow px-2 py-1 rounded-full text-light-gray text-xs'>
-						{tag}
-					</span>
-				))}
-			</div>
-			<div className='flex gap-2'>
-				<button className='button'>See more</button>
-				<Link className='button secondary' href={link} target='_blank' rel='noopener noreferrer'>
-					<span>Go to the page</span>
-					<Icon icon='mingcute:external-link-line' />
-				</Link>
-			</div>
-		</div>
 	);
 }

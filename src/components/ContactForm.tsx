5eed84ee2d8a77b6e3ab6a8de01b5b3892c4f919
Thrillonek <jindrich.kraina@gmail.com>
2026-05-16 15:02:27 +0200

    Added required to some form fields

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -14,16 +14,16 @@ export default function ContactForm() {
 		<>
 			<form onSubmit={(e) => sendEmail(e, { name, email, message })} className='items-center bg-darker shadow-xl p-8 rounded-2xl w-[min(480px,100%)] layout-col-4'>
 				<div>
-					<input onChange={(e) => setName(e.target.value)} type='text' placeholder=' ' id='name' />
+					<input required onChange={(e) => setName(e.target.value)} type='text' placeholder=' ' id='name' />
 					<label htmlFor='name'>Your Name</label>
 				</div>
 				<div>
-					<input onChange={(e) => setEmail(e.target.value)} type='email' placeholder=' ' id='email' />
+					<input required onChange={(e) => setEmail(e.target.value)} type='email' placeholder=' ' id='email' />
 					<label htmlFor='email'>Your Email</label>
 				</div>
 				<div>
 					<textarea onChange={(e) => setMessage(e.target.value)} placeholder=' ' id='message' rows={4} />
-					<label htmlFor='message'>Your Message</label>
+					<label htmlFor='message'>Your Message (optional)</label>
 				</div>
 				<button type='submit' className='form-button button'>
 					Send!

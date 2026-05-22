6d478b91582afae8d6ccd2a1713f443aff682352
Thrillonek <jindrich.kraina@gmail.com>
2026-05-22 19:14:21 +0200

    Updated for vercel migration

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from 'next';
 
-const nextConfig = {
-	output: 'export',
-	images: {
-		unoptimized: true,
-	},
-};
+const nextConfig = {};
 
 module.exports = nextConfig;

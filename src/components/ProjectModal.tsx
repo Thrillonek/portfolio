2c1edd26d2646b98e704d5c9f0fdeb7541d40631
Thrillonek <jindrich.kraina@gmail.com>
2026-05-24 22:08:31 +0200

    Edited copy

--- src/components/ProjectModal.tsx
+++ src/components/ProjectModal.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useTranslations } from 'next-intl';
+import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect } from 'react';
 import { projects } from '../data/projects';
@@ -31,17 +32,20 @@ export default function ProjectModal({ name }: { name: keyof typeof projects }) 
 
 	return (
 		<Modal name={name} title='Project details'>
-			<div className='grid md:grid-cols-2'>
+			<div className='w-2xl'>
 				<div className='p-4 layout-col-4'>
 					<h2>{name}</h2>
 					<p>{t(`projects.${translationProjectNames[name]}.what`)}</p>
-					<div className='flex gap-1'>
+					<div className='flex flex-wrap gap-1'>
 						{projects[name].tags.map((tag) => (
 							<span key={tag} className='bg-dark shadow px-2 py-1 rounded-full text-light-gray text-xs'>
 								{tag}
 							</span>
 						))}
 					</div>
+					{projects[name].images.map((image, idx) => {
+						return <Image key={idx} src={image} alt={name} className='rounded-lg' />;
+					})}
 					<Link href={projects[name].link} className='w-fit button secondary' target='_blank' rel='noopener noreferrer'>
 						{t('projects.goToPage')}
 					</Link>
@@ -49,8 +53,6 @@ export default function ProjectModal({ name }: { name: keyof typeof projects }) 
 				<div className='p-4 layout-col-4'>
 					<h5 className='font-bold'>{t('projects.whyTitle')}</h5>
 					<p>{t(`projects.${translationProjectNames[name]}.why`)}</p>
-					<h5 className='font-bold'>{t('projects.issuesTitle')}</h5>
-					<p>{t(`projects.${translationProjectNames[name]}.issues`)}</p>
 				</div>
 			</div>
 		</Modal>

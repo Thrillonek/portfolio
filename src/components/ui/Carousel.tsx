ca6ebd9827693a68d276804c232c2af71cbeb757
Thrillonek <jindrich.kraina@gmail.com>
2026-06-05 19:11:01 +0200

    Fixed pointer events on phone

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -40,10 +40,13 @@ export default function Carousel({ images }: { images: StaticImageData[] }) {
 		animate(carouselRef.current, { transform: `translateX(0)` }, { duration, easing: currentOffsetRef.current === 0 ? 'ease' : 'ease-out', fill: 'forwards' });
 	}
 
-	function handleMouseMove(e: any, startParams: { pos: number; time?: number }) {
+	function handleMouseMove(e: PointerEvent, startParams: { pos: number; time?: number }) {
+		e.preventDefault();
 		if (!carouselRef.current || !startParams.time) return;
 
-		currentOffsetRef.current = (e.touches ? e.touches[0].clientX : e.clientX) - startParams.pos;
+		console.log('params: ' + Object.entries(startParams).toString());
+
+		currentOffsetRef.current = e.clientX - startParams.pos;
 
 		if (Math.abs(currentOffsetRef.current) > 20) {
 			isCarouselLockedRef.current = false;
@@ -72,14 +75,14 @@ export default function Carousel({ images }: { images: StaticImageData[] }) {
 	});
 
 	return (
-		<div onPointerDown={(e: any) => (startParamsRef.current = { pos: e.touches ? e.touches[0].clientX : e.clientX, time: Date.now() })} className='relative flex items-center rounded-lg w-full h-full overflow-hidden cursor-grab active:cursor-grabbing select-none'>
+		<div onPointerDown={(e: PointerEvent) => (startParamsRef.current = { pos: e.clientX, time: Date.now() })} className='relative flex items-center rounded-lg w-full h-full overflow-hidden cursor-grab active:cursor-grabbing select-none'>
 			<button onClick={(e) => changeImage('left', e)} className='left-2 z-10 absolute bg-neutral-700/50 hover:bg-neutral-700/75 p-1 rounded-full transition-colors'>
 				<Icon icon='mdi:chevron-left' className='text-3xl' />
 			</button>
 			<button onClick={(e) => changeImage('right', e)} className='right-2 z-10 absolute bg-neutral-700/50 hover:bg-neutral-700/75 p-1 rounded-full transition-colors'>
 				<Icon icon='mdi:chevron-right' className='text-3xl' />
 			</button>
-			<div ref={carouselRef} style={{ aspectRatio: `${images[0].width} / ${images[0].height}` }} className='relative flex gap-2 w-full'>
+			<div ref={carouselRef} style={{ aspectRatio: `${images[0].width} / ${images[0].height}` }} className='relative flex gap-2 w-full touch-none'>
 				{Array.from({ length: 3 }, (_, idx) => idx - 1).map((idx) => {
 					const image = images[(currentIndex + idx + images.length) % images.length];
 					return <Image style={{ left: `${idx * 100}%` }} key={idx} className='absolute inset-0 rounded-lg object-cover pointer-events-none' src={image} alt={t('projects.altImage')} />;

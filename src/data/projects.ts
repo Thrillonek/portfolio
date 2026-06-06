9cd12b969c5dae1387568112c0425dbffd98c18b
Thrillonek <jindrich.kraina@gmail.com>
2026-06-06 22:31:53 +0200

    Added new iamges

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,4 +1,9 @@
-import domacnostHrouImage1 from '@/public/domacnost-hrou.png';
+import domacnostHrouImage1 from '@/public/domacnost-hrou/DomacnostHrou1.png';
+import domacnostHrouImage2 from '@/public/domacnost-hrou/DomacnostHrou2.png';
+import domacnostHrouImage3 from '@/public/domacnost-hrou/DomacnostHrou3.png';
+import domacnostHrouImage4 from '@/public/domacnost-hrou/DomacnostHrou4.png';
+import domacnostHrouImage5 from '@/public/domacnost-hrou/DomacnostHrou5.png';
+import domacnostHrouImage6 from '@/public/domacnost-hrou/DomacnostHrou6.png';
 import poznavackaImage1 from '@/public/poznavacka/Poznavacka1.png';
 import poznavackaImage2 from '@/public/poznavacka/Poznavacka2.png';
 import poznavackaImage3 from '@/public/poznavacka/Poznavacka3.png';
@@ -17,6 +22,6 @@ export const projects = {
 		link: 'https://domacnosthrou.cz',
 		tags: ['Next.js', 'React', 'TypeScript', 'PHP', 'Tailwind CSS'],
 		translationAlias: 'domacnostHrou',
-		images: [domacnostHrouImage1],
+		images: [domacnostHrouImage1, domacnostHrouImage2, domacnostHrouImage3, domacnostHrouImage4, domacnostHrouImage5, domacnostHrouImage6],
 	},
 };

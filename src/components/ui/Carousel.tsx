b4629d0acb4befe441177819b3af9bc70f329cdc
Thrillonek <jindrich.kraina@gmail.com>
2026-06-11 18:24:21 +0200

    Made the gap between carousel controls smaller

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -91,7 +91,7 @@ export default function Carousel({ images }: { images: StaticImageData[] }) {
 					})}
 				</div>
 			</div>
-			<div className='flex gap-4'>
+			<div className='flex gap-3'>
 				{Array.from({ length: images.length }, (_, i) => i).map((idx) => (
 					<button onClick={() => setCurrentIndex(idx)} className={clsx('bg-neutral-600 rounded-full h-3 aspect-square transition-transform', currentIndex === idx ? 'scale-150' : 'hover:scale-125')}></button>
 				))}

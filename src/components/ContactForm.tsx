7286bb4be777bf598cc28fd35d714db701bb708d
Thrillonek <jindrich.kraina@gmail.com>
2026-06-11 18:19:27 +0200

    Centered the title in form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -16,7 +16,7 @@ export default function ContactForm() {
 	return (
 		<>
 			<form onSubmit={(e) => sendEmail(e, { name, email, message })} className='items-stretch bg-darker shadow-xl p-8 rounded-2xl w-[min(480px,100%)] layout-col-4'>
-				<h4 className='text-start'>{t('contact.formTitle')}</h4>
+				<h4 className='text-center'>{t('contact.formTitle')}</h4>
 				<div>
 					<input required onChange={(e) => setName(e.target.value)} type='text' placeholder=' ' id='name' />
 					<label htmlFor='name'>{t('contact.name')}</label>
